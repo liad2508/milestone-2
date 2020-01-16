6e6bb2a3278277aa81b4cd32f1c605393dbfb328
liad2508 <58739913+liad2508@users.noreply.github.com>
2020-01-16 18:07:54 +0200

    Update Vertex.h

--- searchable/Vertex.h
+++ searchable/Vertex.h
@@ -9,13 +9,17 @@
 #include <string>
 #include <cstring>
 #include "State.h"
-#include "Point.h"
+#include "myPoint.h"
 
 class Vertex: public State<myPoint*> {
 public:
     Vertex(myPoint* st, double c):State(st,c){}
     int equals(State<myPoint*>* target) {
         return (this->getState()->getX() == target->getState()->getX() && this->getState()->getY() == target->getState()->getY());
+    }
+    bool operator()(State<myPoint *> *state1, State<myPoint *> *state2) {
+
+        return (state1->getCostOfWay() < state2->getCostOfWay());
     }
     string toString();
 };
